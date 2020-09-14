b672d6435e1b642939f413463bb30838eb181a52
Jordan-Gillispie <60756061+Jordan-Gillispie@users.noreply.github.com>
2020-09-13 22:03:56 -0700

    add initial files

---
+++ script.js
@@ -0,0 +1,9 @@
+//step 1 select objects on screen & define globals//
+
+//step 2 add class 'up'//
+
+//step 3 dynamically change text content to countdown//
+
+//step 4 adding event listeners//
+
+//step 5 keep track of score with forEach//
